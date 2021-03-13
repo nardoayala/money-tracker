769c81bd7b0a599347a869b7051576fa82bff45e
Bernardo <ba@bernardoayala.com>
2021-03-13 12:45:35 -0400

    Copyright message edited

--- src/layout/components/Footer.jsx
+++ src/layout/components/Footer.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import icons8Logo from 'Images/icons8.png';
 import './Footer.scss';
 
 const Footer = () => (
   <footer className="footer">
     <div className="footer__container">
       <div className="footer__copyright">
-        <span>Built with 🤍 by </span>
-        <a href="https://twitter.com/nardoyala">nardoyala</a>
+        <span>Icons by: </span>
+        <a href="https://icons8.com/">Icons8</a>
       </div>
-      <ul className="footer__linking">
-        <li>
-          <a href="https://icons8.com/" title="Icons by Icons8">
-            <img src={icons8Logo} alt="Icons8 logo" />
-          </a>
-        </li>
-      </ul>
     </div>
   </footer>
 );
