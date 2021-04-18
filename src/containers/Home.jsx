bc41f8ad09baa10bcb6b7b3b455c97e42a9c6390
Bernardo Ayala <ba@bernardoayala.com>
2021-04-17 20:08:20 -0400

    Prettier added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import MonthSelector from 'Components/MonthSelector';
 import PieChart from 'Components/PieChart';
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './routes/App';
 import 'normalize.css';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
