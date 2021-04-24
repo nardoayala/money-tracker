61300dde7469c47f7a445a45f561c7e0d18b5a4f
Bernardo Ayala <ba@bernardoayala.com>
2021-04-24 13:04:53 -0400

    Chart.js replaced by amcharts

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,61 +1,80 @@
 import React, { Component } from 'react';
-import Chart from 'chart.js/auto';
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+// eslint-disable-next-line camelcase
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 import 'Styles/components/PieChart.scss';
 
+am4core.useTheme(am4themes_animated);
+
 class PieChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { category: 'Food', value: 150 },
+        { category: 'Subscriptions', value: 17.22 },
+        { category: 'Liquor', value: 15 },
+        { category: 'Tech', value: 80 },
+        { category: 'Pets', value: 25 },
+        { category: 'Books', value: 12 },
+      ],
+    };
+  }
+
   componentDidMount() {
-    const ctx = document.getElementById('pieChart').getContext('2d');
-    const pieChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true,
-            display: false,
-          },
-          x: {
-            display: false,
-          },
-        },
-      },
-    });
+    this.generateChart();
+  }
+
+  generateChart() {
+    const { data } = this.state;
+
+    const container = am4core.create('chartdiv', am4core.Container);
+    container.width = am4core.percent(100);
+    container.height = am4core.percent(100);
+    container.layout = 'horizontal';
+
+    // Create chart instance
+    const chart = container.createChild(am4charts.PieChart);
+
+    // Add data
+    chart.data = data;
+
+    // Add Legend
+    chart.legend = new am4charts.Legend();
+    chart.legend.position = 'bottom';
+    chart.legend.maxHeight = 150;
+    chart.legend.scrollable = true;
+    chart.legend.equalWidths = true;
+
+    // Add and configure Series
+    const pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = 'value';
+    pieSeries.dataFields.category = 'category';
+    pieSeries.slices.template.stroke = am4core.color('#fff');
+    pieSeries.slices.template.strokeOpacity = 1;
+    pieSeries.slices.template.tooltipText = '{category} : {value.percent}%';
+    pieSeries.labels.template.disabled = true;
+    pieSeries.colors.list = [
+      am4core.color('rgba(255, 99, 132, 1)'),
+      am4core.color('rgba(54, 162, 235, 1)'),
+      am4core.color('rgba(255, 206, 86, 1)'),
+      am4core.color('rgba(75, 192, 192, 1)'),
+      am4core.color('rgba(153, 102, 255, 1)'),
+      am4core.color('rgba(255, 159, 64, 1)'),
+    ];
+
+    // This creates initial animation
+    pieSeries.hiddenState.properties.opacity = 1;
+    pieSeries.hiddenState.properties.endAngle = -90;
+    pieSeries.hiddenState.properties.startAngle = -90;
+
+    chart.hiddenState.properties.radius = am4core.percent(0);
   }
 
   render() {
-    return (
-      <div className="pie-chart">
-        <div className="pie-chart__container">
-          <canvas id="pieChart" width="400" height="400" />
-        </div>
-      </div>
-    );
+    this.generateChart();
+    return <div id="chartdiv" className="pie-chart" />;
   }
 }
 
